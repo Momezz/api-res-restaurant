5410d6eca77e6b4f3e036150af7eb8d752d5a05d
Julian Gomez <julgomez14@gmail.com>
2023-03-26 19:19:50 -0500

    I remove spaces

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -11,14 +11,9 @@ import {
 const router = Router();
 
 router.get('/', handleAllGetUsers);
-
 router.get('/:id', handleGetUser);
-
 router.post('/', handleCreateUser);
-
 router.patch('/:id', handleUpdateUser);
-
 router.delete ('/:id', handleDeleteUser);
-
 
 export default router;
